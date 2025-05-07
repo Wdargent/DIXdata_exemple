3b87e6e04cd3b46599df2b6a06dc29aedd249bb7
Wdargent <imagedocazerty@gmail.com>
2025-05-07 17:35:08 +0200

    maj bouton edit + nouveau bouton ajout

--- resources/js/pages/IncidentList.jsx
+++ resources/js/pages/IncidentList.jsx
@@ -33,7 +33,10 @@ const IncidentList = () => {
               <path d="M1 2.5A.5.5 0 011.5 2h13a.5.5 0 010 1h-13a.5.5 0 01-.5-.5zM1 5a.5.5 0 01.5-.5h13a.5.5 0 010 1h-13a.5.5 0 01-.5-.5zM1 7.5A.5.5 0 011.5 7h13a.5.5 0 010 1h-13a.5.5 0 01-.5-.5zM1 10a.5.5 0 01.5-.5h13a.5.5 0 010 1h-13a.5.5 0 01-.5-.5zM1 12.5A.5.5 0 011.5 12h13a.5.5 0 010 1h-13a.5.5 0 01-.5-.5z"/>
             </svg>
           </span>
-          Liste des Incidents
+          <div>Liste des Incidents</div>
+          <a class="new-btn" href={`/incidents/new`}>
+            <div>Créer un Incident</div>
+          </a>
         </h1>
       </div>
       <div class="table-wrapper">
@@ -94,7 +97,11 @@ const IncidentList = () => {
                 <td class="table-cell">{incident.visible ? 'Oui' : 'Non'}</td>
                 <td class="table-cell">
                   <a href={`/incidents/${incident.id}/edit`}>
-                    <div class="edit-btn">🖉</div>
+                    <div class="edit-btn icon-btn">
+                      <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="16" height="16">
+                        <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04a1 1 0 000-1.41l-2.34-2.34a1 1 0 00-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/>
+                      </svg>
+                    </div>
                   </a>
                 </td>
             </tr>
