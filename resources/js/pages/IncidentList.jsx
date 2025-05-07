04020a1612a514b0c31d1f66b1e2a702af5eaa10
Wdargent <imagedocazerty@gmail.com>
2025-05-07 15:33:46 +0200

    ajout bordure et bouton edit

--- resources/js/pages/IncidentList.jsx
+++ resources/js/pages/IncidentList.jsx
@@ -27,30 +27,36 @@ const IncidentList = () => {
   return (
     <div>
       <h1>Liste des Incidents</h1>
-      <table border="1" cellPadding="10" class="table-custom">
-        <thead class="table-header">
-          <tr>
-            <th class="table-cell">Titre</th>
-            <th class="table-cell">Description</th>
-            <th class="table-cell">Créé le</th>
-            <th class="table-cell">Modifié le</th>
-            <th class="table-cell">Visible</th>
-            <th class="table-cell">Actions</th>
-          </tr>
-        </thead>
-        <tbody class="table-body">
-          {incidents.map((incident) => (
-            <tr class="table-row" key={incident.id}>
-              <td class="table-cell">{incident.titre}</td>
-              <td class="table-cell">{incident.description}</td>
-              <td class="table-cell">{new Date(incident.created_at).toLocaleString()}</td>
-              <td class="table-cell">{new Date(incident.updated_at).toLocaleString()}</td>
-              <td class="table-cell">{incident.visible ? 'Oui' : 'Non'}</td>
-              <td class="table-cell"><a href={`/incidents/${incident.id}/edit`}>🖉</a></td>
+      <div class="table-wrapper">
+        <table class="table-custom">
+          <thead class="table-header">
+            <tr>
+              <th class="table-cell">Titre</th>
+              <th class="table-cell">Description</th>
+              <th class="table-cell">Créé le</th>
+              <th class="table-cell">Modifié le</th>
+              <th class="table-cell">Visible</th>
+              <th class="table-cell">Actions</th>
+            </tr>
+          </thead>
+          <tbody class="table-body">
+            {incidents.map((incident) => (
+              <tr class="table-row" key={incident.id}>
+                <td class="table-cell">{incident.titre}</td>
+                <td class="table-cell">{incident.description}</td>
+                <td class="table-cell">{new Date(incident.created_at).toLocaleString()}</td>
+                <td class="table-cell">{new Date(incident.updated_at).toLocaleString()}</td>
+                <td class="table-cell">{incident.visible ? 'Oui' : 'Non'}</td>
+                <td class="table-cell">
+                  <a href={`/incidents/${incident.id}/edit`}>
+                    <div class="edit-btn">🖉</div>
+                  </a>
+                </td>
             </tr>
           ))}
         </tbody>
       </table>
+      </div>
     </div>
   );
 };
