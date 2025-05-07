e3aacabac23559bce7765e053f13da6e41852abc
Wdargent <imagedocazerty@gmail.com>
2025-05-07 16:48:25 +0200

    ajout d'icone

--- resources/js/pages/IncidentList.jsx
+++ resources/js/pages/IncidentList.jsx
@@ -27,17 +27,60 @@ const IncidentList = () => {
   return (
     <div>
       <div class="title_table">
-        <h1 class="title_header">Liste des Incidents</h1>
+        <h1 class="title_header">
+          <span class="icon">
+            <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="16" height="16">
+              <path d="M1 2.5A.5.5 0 011.5 2h13a.5.5 0 010 1h-13a.5.5 0 01-.5-.5zM1 5a.5.5 0 01.5-.5h13a.5.5 0 010 1h-13a.5.5 0 01-.5-.5zM1 7.5A.5.5 0 011.5 7h13a.5.5 0 010 1h-13a.5.5 0 01-.5-.5zM1 10a.5.5 0 01.5-.5h13a.5.5 0 010 1h-13a.5.5 0 01-.5-.5zM1 12.5A.5.5 0 011.5 12h13a.5.5 0 010 1h-13a.5.5 0 01-.5-.5z"/>
+            </svg>
+          </span>
+          Liste des Incidents
+        </h1>
       </div>
       <div class="table-wrapper">
         <table class="table-custom">
           <thead class="table-header">
             <tr>
-              <th class="table-cell">Titre</th>
+              <th class="table-cell">
+                <div class="header-with-icon">
+                  <span class="icon_table">
+                  <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="16" height="16">
+                    <path d="M4 1a1 1 0 00-1 1v12a1 1 0 001 1h8a1 1 0 001-1V5.5L9.5 1H4zm5 1.5L12.5 6H9a1 1 0 01-1-1V2.5zM5 9.5a.5.5 0 01.5-.5h5a.5.5 0 010 1h-5a.5.5 0 01-.5-.5zm.5 2a.5.5 0 000 1h5a.5.5 0 000-1h-5z"/>
+                  </svg>
+                  </span>
+                  <div>Titre</div>
+                </div>
+              </th>
               <th class="table-cell">Description</th>
-              <th class="table-cell">Créé le</th>
-              <th class="table-cell">Modifié le</th>
-              <th class="table-cell">Visible</th>
+              <th class="table-cell">
+                <div class="header-with-icon">
+                  <span class="icon_table">
+                    <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="16" height="16">
+                      <path d="M3 0a.5.5 0 010 1h-.5A1.5 1.5 0 001 2.5v11A1.5 1.5 0 002.5 15h11a1.5 1.5 0 001.5-1.5v-11A1.5 1.5 0 0013.5 1H13a.5.5 0 010-1 .5.5 0 010 1h-10a.5.5 0 010-1zM2 4v9.5a.5.5 0 00.5.5h11a.5.5 0 00.5-.5V4H2z"/>
+                    </svg>
+                  </span>
+                  <div>Crée le</div>
+                </div>
+              </th>
+              <th class="table-cell">
+                <div class="header-with-icon">
+                  <span class="icon_table">
+                    <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="16" height="16">
+                      <path d="M6 2a1 1 0 000 2h8a1 1 0 100-2H6zM4 5a2 2 0 00-2 2v9a2 2 0 002 2h5.586a2 2 0 001.414-.586l4.414-4.414A2 2 0 0016 12.414V7a2 2 0 00-2-2H4zm8.5 6.5l1 1-3.086 3.086a.5.5 0 01-.168.11l-1.414.5.5-1.414a.5.5 0 01.11-.168L12.5 11.5z"/>
+                    </svg>
+                  </span>
+                  <div>Modifié le</div>
+                </div>
+              </th>
+              <th class="table-cell">
+                <div class="header-with-icon">
+                  <span class="icon_table">
+                    <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zm-8 4a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm0-1.5A2.5 2.5 0 1 0 8 5a2.5 2.5 0 0 0 0 5z"/>
+                    </svg>
+                  </span>
+                  <div>Visible</div>
+                </div>
+              </th>
               <th class="table-cell">Actions</th>
             </tr>
           </thead>
